01ed5f028af1dbaa872af8edd389c7bf01273c7b
theuscovidatlas <dhalpern@uchicago.edu>
2021-11-08 10:40:45 -0600

    Update app.py

--- atlas_api/app.py
+++ atlas_api/app.py
@@ -7,25 +7,27 @@ def getClient():
 
 def queryGbq(client, query):
     query_job = client.query(query)
-    return query_job.result()
+    return query_job.result().to_dataframe(create_bqstorage_client=False)
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
-
     """
 
     try:
         client = getClient()
         query = 'SELECT * FROM `covid-atlas.public.chr_life` LIMIT 100'
         results = queryGbq(client, query)
-        
+        if True:
+            formatted_results = results.to_json(orient='records')
+
     except Exception as e:
         # Send some context about this error to Lambda Logs
         print(e)
 
+    # print(results.head())
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "data": results
+            "data": formatted_results
         }),
     }
