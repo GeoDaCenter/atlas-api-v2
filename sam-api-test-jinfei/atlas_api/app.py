b20dba14dd0b93ceca549035b67c929bb34cdcce
Jinfei Zhu <jinfei@uchicago.edu>
2021-12-03 09:29:09 -0600

    proxy lambda_handler for greedy path

--- sam-api-test-jinfei/atlas_api/app.py
+++ sam-api-test-jinfei/atlas_api/app.py
@@ -1,11 +1,8 @@
+from datetime import datetime, timedelta, date
+import json
+
 from google.oauth2 import service_account
 from google.cloud import bigquery
-import json
-from urllib.parse import urlparse
-import re
-from datetime import datetime, timedelta
-
-# import pandas as pd
 
 def getClient(path='credentials.json'):
     return bigquery.Client(credentials=service_account.Credentials.from_service_account_file(path))
@@ -20,7 +17,9 @@ def lambda_handler(event, context):
 
     try:
         client = getClient(path='credentials.json')
-        query = build_query_by_parsing(event['requestContext']['path'])
+        path = event["path"]
+        param = event["queryStringParameters"]
+        query = build_query_by_parsing(path, param)
         results = queryGbq(client, query)
         if True:
             formatted_results = results.to_json(orient='records')
@@ -37,49 +36,83 @@ def lambda_handler(event, context):
         }),
     }
 
-def build_query_by_parsing(url):
+def build_query_by_parsing(path, param):
     '''
     Test example:
-    dataset = 'vaccination'
-    spatial_unit = 'county'
-    temporal_unit = 'cumulative'
-    date_range_filter = '?from=2020-05-01&to=2020-11-01'
-    geoid_filter = '?geoid=(1131, 4031, 6063, 6091)'
-    url = f'api.uscovidatlas.org/{dataset}/{spatial_unit}/{temporal_unit}{date_range_filter}{geoid_filter}'
+    path is required, param is optional
+    path = '/vaccination/county/cumulative',
+    param = {'source': 'cdc',
+           'from': '2020-05-01',
+           'geoid': '(1131,204031,206063,206091)',
+           'to': '2020-11-01'}
     '''
-    parsed = urlparse(url)
-    path = parsed.path
-    # split the path into list, like: ['api.uscovidatlas.org', 'vaccination', 'county', 'cumulative']
-    path_lst = parsed.path.split('/')
+    path_lst = path.split('/')
     dataset, spatial_unit, temporal_unit = [i for i in path_lst[1:]]
     
-    # split the query into list
-    filter_lst = parsed.query.split('?')
-    for i in filter_lst:
-        if i.startswith('from'):
-            pattern = r'(\d{4}-\d{2}-\d{2})'
-            start, end = [datetime.fromisoformat(t) for t in re.findall(pattern, i)]
-            
-            date_list = [(start + timedelta(days=x)).strftime("_%Y_%m_%d") for x in range((end - start).days)]
-            columns = (', ').join(date_list)
-
-        if i.startswith('geoid'):
-            # from format 'geoid=(1131, 4031, 6063, 6091)']
-            geo_lst = i[6:]
+    select_clause = create_select_clause(temporal_unit, param)
     
-    sql = f"""
-            SELECT  fips, {columns}
-            FROM `covid-atlas.public.{dataset}_{spatial_unit}_{temporal_unit}`
-            WHERE fips in {geo_lst} 
-            """
-    return sql
+    # default data source is cdc
+    if "source" in param:
+        from_clause = create_from_clause(path, param["source"])
+    else:
+        from_clause = create_from_clause(path, "cdc") 
+    
+    query = select_clause  + from_clause
+    
+    if "geoid" in param:
+        where_clause = f" WHERE fips_code in {param['geoid']}"
+        query += where_clause
 
+    return query
 
+def create_from_clause(path, source):
+    '''
+    source: datasource
+    
+    '''
+    path_lst = path.split('/')
+    dataset, spatial_unit, temporal_unit = [i for i in path_lst[1:]]
+    
+    from_clause = " FROM covid-atlas.public."
+    
+    if dataset == "vaccination":
+        from_clause += ("vaccination_fully_vaccinated" + f"_{source}")
+    elif dataset == "confirmed":
+        from_clause += ("covid_confirmed" + f"_{source}")
+    elif dataset == "deaths":
+        from_clause += ("covid_deaths" + f"_{source}")
+    
+    if spatial_unit == "state":
+        from_clause += "_state"
+        
+    return from_clause
 
+def create_select_clause(temporal_unit, param):
+    '''
+    Input:
+    temporal_unit(str): cumulative, time-series, snapshot
+    param(dic): queryStringParameters
+    Rules:
+    if temporal_unit is snapshot, then 'recentX' (recent x days) in param
+    if "from" in param, "to" is in param
+    if temporal_unit is snapshot, it's stronger than "from" and "to"
+    '''
+    
+    select_clause = "SELECT fips_code"
+    
+    if temporal_unit == "snapshot":
+        end = date.today()
+        start = end - timedelta(days=param['recentX'])
+        date_list = [(start + timedelta(days=x)).strftime("_%Y_%m_%d") for x in range((end - start).days)]
+        columns = (', ').join(date_list)
+        select_clause += f", {columns}"
+        
+    elif "from" in param:
+        start = datetime.fromisoformat(param["from"])
+        end = datetime.fromisoformat(param["to"])
+        date_list = [(start + timedelta(days=x)).strftime("_%Y_%m_%d") for x in range((end - start).days)]
+        columns = (', ').join(date_list)
+        select_clause += f", {columns}"
+        
+    return select_clause    
 
-# # for local testing
-# if __name__ == "__main__":
-#     client = getClient(path='atlas_api/credentials.json')
-#     query = 'SELECT * FROM `covid-atlas.public.chr_life` LIMIT 100'
-#     results = queryGbq(client, query)
-#     print(results.head())
